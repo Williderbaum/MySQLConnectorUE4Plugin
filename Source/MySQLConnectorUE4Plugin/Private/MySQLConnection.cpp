8e7b0dcdd1759497a9491faf0c22ddb4bdc1a08f
Williderbaum <59510119+Williderbaum@users.noreply.github.com>
2023-10-05 12:32:07 +0200

    remove  blueprint utility

--- Source/MySQLConnectorUE4Plugin/Private/MySQLConnection.cpp
+++ Source/MySQLConnectorUE4Plugin/Private/MySQLConnection.cpp
@@ -2,12 +2,7 @@
 #include "MySQLConnectorUE4Plugin.h"
 
 
-UMySQLConnection::UMySQLConnection(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-}
-
-bool UMySQLConnection::MySQLCheckConnection()
+bool FMySQLConnection::MySQLCheckConnection()
 {
 	if (globalCon)
 	{
@@ -16,22 +11,188 @@ bool UMySQLConnection::MySQLCheckConnection()
 	return false;
 }
 
-bool UMySQLConnection::MySQLCloseConnection(UMySQLConnection* Connection)
+bool FMySQLConnection::MySQLCloseConnection()
 {
-	if (Connection){
-		if (Connection->MySQLCheckConnection())
-		{
-			mysql_close(Connection->globalCon);
-			Connection->globalCon = nullptr;
-			mysql_library_end();
-			return true;
-		}else
-		{
-			UE_LOG(LogTemp, Error, TEXT("MySQLCloseConnection: Connection is valid but Server does no respond!"));
-		}
-	} else
+	
+	if (MySQLCheckConnection())
 	{
-		UE_LOG(LogTemp, Error, TEXT("MySQLCloseConnection: Connection is null!"));
+		mysql_close(globalCon);
+		globalCon = nullptr;
+		mysql_library_end();
+		return true;
 	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("MySQLCloseConnection: Connection is valid but Server does no respond!"));
+	}
+	
+	
 	return false;
+}
+
+bool FMySQLConnection::MySQLInitConnection(FString Host, FString UserName, FString UserPassword, FString DatabaseName)
+{
+	std::string HostString(TCHAR_TO_UTF8(*Host));
+	std::string UserNameString(TCHAR_TO_UTF8(*UserName));
+	std::string UserPasswordString(TCHAR_TO_UTF8(*UserPassword));
+	std::string DatabaseNameString(TCHAR_TO_UTF8(*DatabaseName));
+
+	if (mysql_library_init(0, nullptr, nullptr) != 0)
+	{
+		UE_LOG(LogTemp, Error, TEXT("MySQLInitConnection: FAILED TO INIT mysql library"));
+		GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Cyan, TEXT("FAILED TO INIT mysql library"));
+		mysql_library_end();
+		return false;
+	}
+
+	globalCon = mysql_init(nullptr);
+	if (!globalCon)
+	{
+		UE_LOG(LogTemp, Error, TEXT("MySQLInitConnection: FAILED TO INIT connection"));
+		mysql_library_end();
+		return false;
+	}
+
+	if (!mysql_real_connect(globalCon,
+		HostString.c_str(),
+		UserNameString.c_str(),
+		UserPasswordString.c_str(),
+		DatabaseNameString.c_str(),
+		0, nullptr, 0))
+	{
+		UE_LOG(LogTemp, Error, TEXT("MySQLInitConnection: Failed to Connect to Database!"));
+		MySQLCloseConnection();
+		return false;
+	}
+
+	if (mysql_set_character_set(globalCon, "utf8") != 0)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Cyan, TEXT("MySQLConnector: Can't set UTF-8 with mysql_set_character_set"));
+		UE_LOG(LogTemp, Error, TEXT("MySQLInitConnection: Can't set UTF-8 with mysql_set_character_set"));
+		MySQLCloseConnection();
+		return false;
+	}
+
+	return true;
+}
+
+bool FMySQLConnection::MySQLConnectorExecuteQuery(FString Query)
+{
+	if (!globalCon)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Connection is NULL!"));
+		return false;
+	}
+
+	std::string MyStdString(TCHAR_TO_UTF8(*Query));
+
+	//if (mysql_query(con, "INSERT INTO `test`.`t1` (`Qwe`) VALUES ('test ');")) {
+	if (mysql_query(globalCon, MyStdString.c_str()))
+	{
+		return false;
+	}
+	return true;
+}
+
+MySQLConnectorQueryResult FMySQLConnection::RunQueryAndGetResults(const FString& Query)
+{
+	MySQLConnectorQueryResult resultOutput;
+	
+	if (!MySQLCheckConnection())
+	{
+		UE_LOG(LogTemp, Error, TEXT("Not connected!"));
+		resultOutput.ErrorMessage = "Not connected!";
+		resultOutput.Success = false;
+		return resultOutput;
+	};
+
+	//////////////////////////////////////////////////////////////////////////
+	// Get and assign the data
+	//////////////////////////////////////////////////////////////////////////
+
+	std::string MyStdString(TCHAR_TO_UTF8(*Query));
+
+	if (mysql_query(globalCon, MyStdString.c_str())) // "SELECT * FROM Cars"
+	{
+		//finish_with_error(con);
+	}
+
+	MYSQL_RES* result = mysql_store_result(globalCon);
+
+	if (!result)
+	{
+		//finish_with_error(con);		
+		UE_LOG(LogTemp, Error, TEXT("Result is NULL!"));
+		resultOutput.ErrorMessage = "Result is NULL!";
+		resultOutput.Success = false;
+		return resultOutput;
+	}
+
+	int num_fields = mysql_num_fields(result);
+
+	TArray<int> fieldTypes;
+	TArray<FString> fieldNames;
+
+	MYSQL_FIELD* fields;
+	fields = mysql_fetch_fields(result);
+	if (fields)
+	{
+		for (int i = 0; i < num_fields; i++)
+		{
+			FString NewString = FString::FromInt(fields[i].type);
+			//GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Cyan, TEXT("MySQLConnector: Type is:") + NewString);
+
+			fieldTypes.Add(fields[i].type);
+			fieldNames.Add(UTF8_TO_TCHAR(fields[i].name));
+		}
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	// Get and assign the data
+	//////////////////////////////////////////////////////////////////////////
+
+	TArray<MySQLConnectorResultValue> resultRows;
+
+	MYSQL_ROW row;
+	while ((row = mysql_fetch_row(result)))
+	{
+		MySQLConnectorResultValue rowVal;
+
+		for (int i = 0; i < num_fields; i++)
+		{
+			MySQLConnectorResultField val;
+
+			FString columnNameStr = fieldNames[i];
+			val.Name = columnNameStr;
+
+			FString fieldValueStr = (UTF8_TO_TCHAR(row[i]));
+
+			switch (fieldTypes[i])
+			{
+			case enum_field_types::MYSQL_TYPE_LONG:
+				val.Type = MySQLConnectorResultValueTypes::Int;
+				val.IntValue = FCString::Atoi(*fieldValueStr);
+				break;
+			case enum_field_types::MYSQL_TYPE_VAR_STRING:
+				val.Type = MySQLConnectorResultValueTypes::Varchar;
+				val.StringValue = fieldValueStr;
+				break;
+			default:
+				val.Type = MySQLConnectorResultValueTypes::UnsupportedValueType;
+				val.IntValue = FCString::Atoi(*fieldValueStr);
+				val.StringValue = fieldValueStr;
+			}
+
+			rowVal.Fields.Add(val);
+		}
+
+		resultRows.Add(rowVal);
+	}
+
+	mysql_free_result(result);
+
+	resultOutput.Results = resultRows;
+	resultOutput.Success = true;
+
+	return resultOutput;
 }
--- Source/MySQLConnectorUE4Plugin/Public/MySQLConnection.h
+++ Source/MySQLConnectorUE4Plugin/Public/MySQLConnection.h
@@ -2,25 +2,74 @@
 
 #include  "mysql.h"
 
-#include "MySQLConnection.generated.h"
 
-/**
-* SQLite main database class.
-*/
-UCLASS()
-class MYSQLCONNECTORUE4PLUGIN_API UMySQLConnection : public UObject
+// A few things for internal use here.
+namespace MySQLConnectorResultValueTypes
 {
-	GENERATED_UCLASS_BODY()    
+	enum MySQLConnectorResultValType
+	{
+		Int,
+		Varchar,
+		UnsupportedValueType
+	};
+}
 
+
+// Result field, used as an intermediary when collecting results from
+// an MySQL query.
+struct MySQLConnectorResultField
+{
+	FString StringValue;
+	double DoubleValue;
+	int64 IntValue;
+
+	FString Name;
+	MySQLConnectorResultValueTypes::MySQLConnectorResultValType Type;
+
+	FString ToString()
+	{
+		if (Type == MySQLConnectorResultValueTypes::Varchar)
+			return StringValue;
+		if (Type == MySQLConnectorResultValueTypes::Int)
+			return FString::Printf(TEXT("%i"), IntValue);
+		return StringValue;
+	}
+};
+
+struct MySQLConnectorResultValue
+{
+	TArray<MySQLConnectorResultField> Fields;
+};
+
+// The internal result object.
+struct MySQLConnectorQueryResult
+{
+	bool Success;
+	FString ErrorMessage;
+	TArray<MySQLConnectorResultValue> Results;
+};
+
+
+
+
+
+class MYSQLCONNECTORUE4PLUGIN_API FMySQLConnection
+{
+	
 public:
 
     MYSQL* globalCon = nullptr;
    
-    UFUNCTION(BlueprintCallable, Category = "MySQLConnector|Connection")
-		bool MySQLCheckConnection();
+	bool MySQLCheckConnection();
 
 	// checks if the connection is valid and closes it, resets the pointers to nullptr on success
 	// true on success, false in case the connection is not established or the argument is nullptr
-	UFUNCTION(BlueprintCallable, Category = "MySQLConnector|Connection")
-	static bool MySQLCloseConnection(UMySQLConnection* Connection);
+	bool MySQLCloseConnection();
+
+	bool MySQLInitConnection(FString Host, FString UserName, FString UserPassword, FString DatabaseName);
+
+	bool MySQLConnectorExecuteQuery(FString Query);
+
+	/** Get data from the database using a select statement and return the rows. */
+	MySQLConnectorQueryResult RunQueryAndGetResults(const FString& Query);
 };
